8b6b1bcdade2b028a5bdc50730caed38df877e37
lanrenwf <ssi_lavarel@163.com>
2019-10-18 17:19:44 +0800

    1

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -10,7 +10,6 @@ class WechatController extends Controller
 {
     public function getUser(Request $request)
     {
-        echo 123;
         $app = Factory::officialAccount(config('wechat.official_account.default'));
         $oauth = $app->oauth;
         $user = $oauth->user();
