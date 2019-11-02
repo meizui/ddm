c4698e9a643eee2e616871f2448c6c0b931211bd
lanrenwf <ssi_lavarel@163.com>
2019-11-02 11:44:12 +0800

    1

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -37,32 +37,20 @@
 <!-- end navbar top -->
 
 <!-- side nav right-->
-<div class="side-nav-panel-right">
-    <ul id="slide-out-right" class="side-nav side-nav-panel collapsible">
-        <li class="profil">
-            <img src="img/profile2.jpg" alt="">
-            <h2>John Doe</h2>
-        </li>
-        <li><a href="setting.html"><i class="fa fa-cog"></i>Settings</a></li>
-        <li><a href="about-us.html"><i class="fa fa-user"></i>About Us</a></li>
-        <li><a href="contact.html"><i class="fa fa-envelope-o"></i>Contact Us</a></li>
-        <li><a href="login.html"><i class="fa fa-sign-in"></i>Login</a></li>
-        <li><a href="register.html"><i class="fa fa-user-plus"></i>Register</a></li>
-    </ul>
-</div>
+
 <!-- end side nav right-->
 
 <!-- navbar bottom -->
 <div class="navbar-bottom">
     <div class="row">
         <div class="col s2">
-            <a href="index.html"><i class="fa fa-home icon-th-large"></i></a>
+            <a href="/"><i class="fa fa-home icon-th-large"></i></a>
         </div>
         <div class="col s3">
             <a href="shop.html"><i class="fa fa-dot-circle-o"></i></a>
         </div>
         <div class="col s2">
-            <a href="#animatedModal2" id="nav-menu"><i class="fa fa-heart"></i></a>
+            <a href="#animatedModal2" id="nav-menu"><i class="fa fa-plus"></i></a>
         </div>
         <div class="col s2">
             <a href="contact.html"><i class="fa fa-comment-o"></i></a>
