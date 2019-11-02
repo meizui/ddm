0893b3b3bed2f89ac00a810d81f3541b7de09683
lanrenwf <ssi_lavarel@163.com>
2019-11-02 14:54:14 +0800

    1

--- app/Http/Controllers/BaseController.php
+++ app/Http/Controllers/BaseController.php
@@ -12,21 +12,22 @@ class BaseController extends Controller
     //  获取微信信息
     public function __construct(Request $request)
     {
-        $wechatUser = session('wechat.oauth_user.default');
-        $user = User::where('openid',$wechatUser->original['openid'])->first();
+        if (!session('logined')){
+            $wechatUser = session('wechat.oauth_user.default');
+            $user = User::where('openid',$wechatUser->original['openid'])->first();
+            if(!$user) {
+                $user = new User();
+                $user->openid = $wechatUser->original['openid'];
+            }
 
-        if(!$user) {
-            $user = new User();
+            $user->nickname = $wechatUser->original['nickname'];
+            $user->avatar = $wechatUser->original['headimgurl'];
+            $user->sex = $wechatUser->original['sex'];
+            $user->province = $wechatUser->original['province'];
+            $user->city = $wechatUser->original['city'];
+            $user->save();
+            session(['logined'=>true]);
         }
-
-        $user->openid = $wechatUser->original['openid'];
-        $user->nickname = $wechatUser->original['nickname'];
-        $user->avatar = $wechatUser->original['headimgurl'];
-        $user->sex = $wechatUser->original['sex'];
-        $user->province = $wechatUser->original['province'];
-        $user->city = $wechatUser->original['city'];
-        $user->save();
-
     }
 
 }
